3ee01ff1a101b5b25ddee493b368830466a44a9f
DiegoELT99 <diegoelt99@gmail.com>
2020-11-07 16:31:07 -0500

    Now generating a RT in each internal node.

--- rangetree.cpp
+++ rangetree.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
+const long long maxCities = 1e4;
+
 struct Node {
   pair<int, int> xyCoordinates;
   Node * m_pSon[2];
   Node * nextNode;
   Node * prevNode;
+  Node * rootOfOwnRT;
   Node(int x, int y) {
     xyCoordinates = make_pair(x, y);
     m_pSon[0]=0;
     m_pSon[1]=0; 
     nextNode = nullptr;
     prevNode = nullptr;
+    rootOfOwnRT = nullptr;
   }
 };
 
-pair<Node *, Node *> create_range_tree(pair<int, int> v[],int l, int h, Node * lastLeafNode) {
+bool sortbysec(const pair<int,int> &a,  const pair<int,int> &b) { 
+    return (a.second < b.second); 
+} 
+
+pair<Node *, Node *> create_range_tree(pair<int, int> v[],int l, int h, Node * lastLeafNode, bool firstDimension) {
   if(l==h) {
     Node * parent = new Node(v[l].first, v[l].second);
     parent -> m_pSon[0] = new Node(v[l].first, v[l]. second);
@@ -39,13 +47,29 @@ pair<Node *, Node *> create_range_tree(pair<int, int> v[],int l, int h, Node * l
     return make_pair(parent, parent -> m_pSon[1]);
   }
 
+  vector<pair<int, int>> nodesInRange;
+  pair<int, int> nodesInRangeArray[maxCities];
+  // TODO: Create range tree for each internal node
+  
   int m = (l + h)/2;
-  pair<Node *, Node *> pairLeft = create_range_tree(v,l, m, lastLeafNode);
-  pair<Node *, Node *> pairRight = create_range_tree(v,m+1, h, pairLeft.second);
+  pair<Node *, Node *> pairLeft = create_range_tree(v,l, m, lastLeafNode, true);
+  pair<Node *, Node *> pairRight = create_range_tree(v,m+1, h, pairLeft.second, true);
   
   Node * parent = new Node((pairLeft.first -> xyCoordinates.first + pairRight.first -> xyCoordinates.first)/2, (pairLeft.second -> xyCoordinates.second + pairRight.second -> xyCoordinates.second)/2);
   parent -> m_pSon[0] = pairLeft.first; 
   parent -> m_pSon[1] = pairRight.first;
+
+  if (firstDimension) {
+    for (int i = l; i <= h; i++)
+    nodesInRange.emplace_back(v[i]);
+
+    sort(nodesInRange.begin(), nodesInRange.end(), sortbysec);
+
+    for (int i = l; i <= h; i++)
+      nodesInRangeArray[i] = nodesInRange[i];
+
+    parent -> rootOfOwnRT = create_range_tree(nodesInRangeArray, 0, h - l + 1, nullptr, false).first;
+  } 
 
   return make_pair(parent, pairRight.second);
 }
@@ -62,13 +86,13 @@ void print(Node * r)
 int main()
 {
     pair<int,int> v[] = {make_pair(36,37), make_pair(42,51), make_pair(62,72), make_pair(80, 80), make_pair(90, 90), make_pair(100, 100)};
-    Node * root = create_range_tree(v,0,5, nullptr).first;
+    Node * root = create_range_tree(v,0,5, nullptr, true).first;
 
     cout << "Testing both ways of printing.\n";
     
     print(root);
 
     cout << "\n";
-    
+
     return 0;
 }
